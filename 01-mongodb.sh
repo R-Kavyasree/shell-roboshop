a2c8d9d64841cb12fa356da43a681f689390a7fc
R-Kavyasree <kavyasree4221@gmail.com>
2026-08-22 21:36:21 +0530

    api doc added

--- 01-mongodb.sh
+++ 01-mongodb.sh
@@ -1,48 +1,56 @@
 #!/bin/bash
 
 LOGS_FOLDER="/var/log/roboshop"
-sudo mkdir -p $LOGS_FOLDER
-sudo chown -R ec2-user:ec2-user $LOGS_FOLDER
-sudo chmod -R 755 $LOGS_FOLDER
-LOGS_FILE="$LOGS_FOLDER/$0.LOG"
+
+sudo mkdir -p "$LOGS_FOLDER"
+sudo chown -R ec2-user:ec2-user "$LOGS_FOLDER"
+sudo chmod -R 755 "$LOGS_FOLDER"
+
+LOGS_FILE="$LOGS_FOLDER/01-mongodb.sh.LOG"
 
 USERID=$(id -u)
+
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
-N="\e[-m"
+N="\e[0m"
+
 TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
 
-
-if [ $USERID -ne 0 ]; then
-echo -e " $TIMESTAMP [ERROR] $R Please run this script with root access $N" | TEE -a $LOGS_FILE
-exit 1
-fi
-VALIDATE(){
-    if [ $1 -ne 0 ]; then
-    echo -e " $2.... $R FAILURE $N" | tee -a $LOGS_FILE
+if [ "$USERID" -ne 0 ]; then
+    echo -e "$TIMESTAMP [ERROR] $R Please run this script with root access $N" | tee -a "$LOGS_FILE"
     exit 1
+fi
+
+VALIDATE() {
+    if [ "$1" -ne 0 ]; then
+        echo -e "$TIMESTAMP [ERROR] $2.... $R FAILURE $N" | tee -a "$LOGS_FILE"
+        exit 1
     else
-    echo -e "$TIMESTAMP [INFO] $2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
-fi 
-
-
+        echo -e "$TIMESTAMP [INFO] $2 ... $G SUCCESS $N" | tee -a "$LOGS_FILE"
+    fi
 }
+
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 VALIDATE $? "ADDING Mongo repo"
 
-dnf install mongodb-org -y &>> $LOGS_FILE
-VALIDATE $? "Installing Mongodb"
+dnf clean all &>> "$LOGS_FILE"
+VALIDATE $? "Cleaning DNF cache"
 
-systemctl enable --now mongod
-VALIDATE $? "starting and enabling Mongodb"
+dnf makecache &>> "$LOGS_FILE"
+VALIDATE $? "Updating DNF metadata"
 
-systemctl status --now mongod
-VALIDATE $? "status is enabled"
+dnf install mongodb-org -y &>> "$LOGS_FILE"
+VALIDATE $? "Installing MongoDB"
 
+systemctl enable --now mongod &>> "$LOGS_FILE"
+VALIDATE $? "Starting and enabling MongoDB"
+
+systemctl status mongod --no-pager &>> "$LOGS_FILE"
+VALIDATE $? "Checking MongoDB status"
 
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
-VALIDATE $? "allowing remote to Mongodb"
+VALIDATE $? "Allowing remote access to MongoDB"
 
-systemctl restart mongod
-VALIDATE $? "Restarting success"
+systemctl restart mongod &>> "$LOGS_FILE"
+VALIDATE $? "Restarting MongoDB"
