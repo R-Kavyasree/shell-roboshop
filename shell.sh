88404321cd733e6707b64bafb1b4dd96333982f4
R-Kavyasree <kavyasree4221@gmail.com>
2026-08-22 22:14:32 +0530

    api doc added

--- shell.sh
+++ shell.sh
@@ -4,25 +4,14 @@ AMI_ID="ami-0220d79f3f480ecf5"
 ZONE_ID="Z07104562L34RC4JURX7T"
 DOMAIN_NAME="rkdaws90.online"
 
-COMMON_SG="sg-09ea79c74550aed12"
-MONGODB_SG="sg-0dcab715112f73377"
-REDIS_SG="sg-0c500a298f3dd1b92"
-
 for instance in $@
 do
-
     echo "Launching instance: $instance"
-
-    if [ "$instance" == "mongodb" ]; then
-        SECURITY_GROUP="$MONGODB_SG"
-    elif [ "$instance" == "redis" ]; then
-        SECURITY_GROUP="$REDIS_SG"
-    fi
 
     INSTANCE_ID=$(aws ec2 run-instances \
         --image-id "$AMI_ID" \
         --instance-type t3.micro \
-        --security-group-ids "$COMMON_SG" "$SECURITY_GROUP" \
+        --security-groups roboshopcommon "roboshop-$instance" \
         --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=roboshop-$instance}]" \
         --query 'Instances[0].InstanceId' \
         --output text
@@ -30,11 +19,16 @@ do
 
     echo "Instance ID: $INSTANCE_ID"
 
+    echo "Waiting for instance to be running..."
+
+    aws ec2 wait instance-running \
+        --instance-ids "$INSTANCE_ID"
+
     if [ "$instance" == "frontend" ]; then
 
         IP=$(aws ec2 describe-instances \
             --instance-ids "$INSTANCE_ID" \
-            --query 'Reservations[*].Instances[*].PublicIpAddress' \
+            --query 'Reservations[0].Instances[0].PublicIpAddress' \
             --output text
         )
 
@@ -44,7 +38,7 @@ do
 
         IP=$(aws ec2 describe-instances \
             --instance-ids "$INSTANCE_ID" \
-            --query 'Reservations[*].Instances[*].PrivateIpAddress' \
+            --query 'Reservations[0].Instances[0].PrivateIpAddress' \
             --output text
         )
 
@@ -54,6 +48,8 @@ do
 
     echo "IP Address: $IP"
     echo "Route53 Record: $R53_RECORD"
+
+    ### Updating R53 Record ###
 
     aws route53 change-resource-record-sets \
         --hosted-zone-id "$ZONE_ID" \
@@ -65,7 +61,7 @@ do
                     "ResourceRecordSet": {
                         "Name": "'"$R53_RECORD"'",
                         "Type": "A",
-                        "TTL": 30,
+                        "TTL": 1,
                         "ResourceRecords": [
                             {
                                 "Value": "'"$IP"'"
