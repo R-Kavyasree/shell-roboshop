ac3ec5d75020b787c2048b39d4e54d50fc685590
R-Kavyasree <kavyasree4221@gmail.com>
2026-08-22 16:04:18 +0530

    fix ec2 launch script

--- shell.sh
+++ shell.sh
@@ -4,14 +4,25 @@ AMI_ID="ami-0220d79f3f480ecf5"
 ZONE_ID="Z07104562L34RC4JURX7T"
 DOMAIN_NAME="rkdaws90.online"
 
+COMMON_SG="sg-09ea79c74550aed12"
+MONGODB_SG="sg-0dcab715112f73377"
+REDIS_SG="sg-0c500a298f3dd1b92"
+
 for instance in $@
 do
+
     echo "Launching instance: $instance"
+
+    if [ "$instance" == "mongodb" ]; then
+        SECURITY_GROUP="$MONGODB_SG"
+    elif [ "$instance" == "redis" ]; then
+        SECURITY_GROUP="$REDIS_SG"
+    fi
 
     INSTANCE_ID=$(aws ec2 run-instances \
         --image-id "$AMI_ID" \
         --instance-type t3.micro \
-        --security-groups "roboshopcommon" "roboshop-$instance" \
+        --security-group-ids "$COMMON_SG" "$SECURITY_GROUP" \
         --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=roboshop-$instance}]" \
         --query 'Instances[0].InstanceId' \
         --output text
