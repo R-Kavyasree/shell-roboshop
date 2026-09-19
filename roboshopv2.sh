769c44751b7a334dfd755640013c976aff815b4c
R-Kavyasree <kavyasree4221@gmail.com>
2026-09-19 19:47:55 +0530

    api doc added

--- roboshopv2.sh
+++ roboshopv2.sh
@@ -4,6 +4,10 @@ AMI_ID="ami-0220d79f3f480ecf5"
 ZONE_ID="Z07104562L34RC4JURX7T"
 DOMAIN_NAME="rkdaws90.online"
 
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 ## Validation ###
 
 if [ $# -lt 2 ]; then
@@ -15,7 +19,7 @@ fi
    ACTION=$1
    shift # first argument will be removed
 
-   if [" $Action"-ne"create] && [ $ACTION -ne "delete"]; then
+   if [ "$Action" !=  "create" ] && [ "$ACTION" !=  "delete" $N ]; then
    echo "ERROR :: First argument must be either create or delete"
    echo "USAGE: $0 [create/delete] [instance1] [instance2...]"
    exit 1
